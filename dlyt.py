f670167bb725f28f40130bad3404546410e76c87
nightwing <nightwing@kali>
2025-12-14 00:37:17 -0500

    fixing outdated code

--- dlyt.py
+++ dlyt.py
@@ -1,69 +1,183 @@
 from kivymd.app import MDApp
 #from kivymd.uix.button 
 from kivy.lang import Builder 
-from pytube import YouTube
+from pytubefix import YouTube
 from kivymd.uix.scrollview import MDScrollView
 from kivy.properties import ObjectProperty
 import webbrowser
 import kivy
-from kivy.app import App
-
-#from kivymd.uix.screen import MDScreenManager,MDscreen
-
-
-
+from kivymd.uix.screen import MDScreen
+#do loading of kv file here to avoid root error
+#https://pictogrammers.com/library/mdi/
+#relearn multithreading or async so the download can be quick
 """
-*created by:D.wolf
-*simple youtube downloader
+Note: putting things in <is a rule>: meaning dont display it to the app yet just desiging what goes inside it
+until you call it with its name with out the <>
+TASK:
+~ CREATE A BUTTON TO SWITCH FROM AUDIO TO VIDEO FORMAT 
+~ CREATE SNACKBAR TO TELL WHEN FILE FINISHES TO DOWNLOad
+~ TEST
+"""
+
+KV = """
+#MENU SECTION
+<MDTopAppBar>:
+    type: "small"
+    pos_hint: {"center_x": .5, "center_y": .95}
+
+    MDTopAppBarLeadingButtonContainer:
+
+        MDActionTopAppBarButton:
+            icon: "menu"                    
+            on_release: app.open_nav()#get logic to grab id in navigation class
+
+
+
+    MDTopAppBarTitle:
+        text: "DOWNLOADER"
+        halign: "center"
+
+    MDTopAppBarTrailingButtonContainer:
+
+        #MDActionTopAppBarButton:
+           # icon: "download"
+            #on_release: app.download_mp3()
+            #download function for button go here
+#MAIN 
+Downloader:
+    md_bg_color: self.theme_cls.backgroundColor
+    MDTopAppBar:
+
+    MDNavigationLayout:
+
+        MDScreenManager:
+
+            MDScreen: #all contents go here
+            
+                #MDButton: make the download button outside right of text field
+                #    pos_hint:{"center_x":.2}
+                MDTextField:
+                    id:linker
+                    #mode: "filled"
+                    size_hint_x:None
+                    width:"240dp"
+                    pos_hint:{"center_x":.5,"center_y":.5}
+                    on_text_validate:app.download_mp3(linker.text)
+                    
+
+                    MDTextFieldHintText:
+                        text: "https://youtu.be/UEO0alk23kick"
+
+                    MDTextFieldHelperText:
+                        text: "Type link here"
+                        mode: "persistent"
+
+                    #MDTextFieldTrailingIcon:
+                    #    icon: "download"
+
+
+        MDNavigationDrawer:
+            id: nav_drawer
+            radius: 0, dp(16), dp(16), 0
+
+            MDNavigationDrawerMenu:
+
+                MDNavigationDrawerLabel:
+                    text: "Menu"
+
+                MDNavigationDrawerItem:
+                    MDButton:
+                        pos_hint:{"center_y":.5}
+                        on_release:app.instagram()
+                        
+                        MDButtonIcon:
+                            icon:"instagram"
+
+                    MDNavigationDrawerItemText:
+                        text: "instagram"
+
+                    
+                MDNavigationDrawerDivider:
+
+
+                MDNavigationDrawerItem:
+                    MDButton:
+                        pos_hint:{"center_y":.5}
+                        on_release:app.twitter()
+                        
+                        MDButtonIcon:
+                            icon:"twitter"
+
+                    MDNavigationDrawerItemText:
+                        text: "twitter"
+
+                    
+                MDNavigationDrawerDivider:
+
+                MDNavigationDrawerItem:
+                    
+                    MDButton:
+                        pos_hint:{"center_y":.5}
+                        on_release:app.github()
+                        
+                        MDButtonIcon:
+                            icon:"github"
+                    
+
+                    MDNavigationDrawerItemText:
+                        text: "github"
+                    
+                MDNavigationDrawerDivider:
+
+                MDNavigationDrawerItem:
+
+                    MDNavigationDrawerItemLeadingIcon:
+                        icon: "cash"
+
+                    MDNavigationDrawerItemText:
+                        text: "support me ->$dwolf94"
+
+                    
+                MDNavigationDrawerDivider:
+                    MDNavigationDrawerItem:
 """
 
 
+class Downloader(MDScreen):
+    ...
 
-kv = "DL.kv"
-class ContentNav(MDScrollView):
-    screen_manager = ObjectProperty()
-    nav = ObjectProperty()
-
-
-class Test(MDApp):
-    def __init__(self,**kwargs):
-        super().__init__(**kwargs)
-        self.screen = Builder.load_file(kv)
-
+class DownerApp(MDApp):
     def build(self):
-        self.theme_cls.primary_pallete = "Pink"
         self.theme_cls.theme_style = "Dark"
+        self.theme_cls.primary_pallete:"Orange"
+        return Builder.load_string(KV)
+    def open_nav(self):
+        self.root.ids.nav_drawer.set_state("toggle")
 
-        return self.screen
-
-    def audio_func(self,*args):
-        #mp3 func
+    def download_mp3(self,link):
         try:
-            links = self.screen.ids.link.text
-            yt = YouTube(links)
+            yt = YouTube(link)
             audio = yt.streams.get_audio_only().download()
         except Exception as e:
-            print(f"the problem could be {e}")
-        print("done")
+            print(e)
 
-    def video_func(self,*args):
-        #mp4 function
-        try:
-            links = self.screen.ids.link.text
-            utube = YouTube(links)
-            utube = utube.streams.get_highest_resolution().download()
-        except Exception as e:
-            print(f"the problem is {e}")
-        print("finished")
+    #SOCIAL SECTION------------------------------------------------
+    def instagram(self):
+       webbrowser.open("https://www.instagram.com/d.wolf_/")
+
+    def twitter(self):
+       webbrowser.open("x.com/D_W01F")
 
     def github(self):
         webbrowser.open("https://github.com/nightwing25")
 
-    
+    def threads(self):
+        pass #add threads link
 
-if __name__=="__main__":
-    Test().run()
+    def support_me(self):
+       ...
+    #def trouble_shoot(self):
+    #    print(self.root.ids)
 
-
-
-
+if __name__ == "__main__":
+    DownerApp().run()
